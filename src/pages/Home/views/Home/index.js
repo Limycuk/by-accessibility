ab5b063368d6fb87310da5e0be393805374c6966
Baltsevich Alexander <abaltsevich@marathonbet.ru>
2018-09-10 19:58:51 +0300

    added final form

--- src/pages/Home/views/Home/index.js
+++ src/pages/Home/views/Home/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { Form, Field } from "react-final-form";
 
-import { withStyles } from '@material-ui/core/styles';
-import styles from './styles';
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
 
 const Home = ({ classes }) => {
   return (
     <div className={classes.container}>
-      Home
-      <div>test</div>
+      <Form />
     </div>
   );
 };
 
 Home.propTypes = {
   classes: PropTypes.shape({
-    container: PropTypes.string.isRequired,
-  }).isRequired,
+    container: PropTypes.string.isRequired
+  }).isRequired
 };
 
 export default withStyles(styles)(Home);
